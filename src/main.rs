1778fed022c804bf1b0933fc58066f69ed8186c1
ppoum <pam@poum.ca>
2024-12-15 14:33:26 -0500

    Configure proper higher half linking
    Small bootstrap assembly script runs in the lower half, loads the paging
    configured by the bootloader and jumps the the kernel entrypoint (kmain)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 #![no_std]
 #![no_main]
 
+use core::arch::global_asm;
+
 #[panic_handler]
 fn _panic_handler(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+global_asm!(include_str!("main.s"));
+
 #[no_mangle]
-pub extern "C" fn _start() -> usize {
-    0
+pub extern "C" fn kmain(_magic: u32, _mb2_ptr: u32) -> ! {
+    loop {}
 }
---
+++ src/main.s
@@ -0,0 +1,50 @@
+.pushsection .bootstrap, "ax"
+mb2_magic:
+    .space 64
+mb2_ptr:
+    .space 64
+.globl _start
+_start:
+    // Save for OS
+    mov [rip +mb2_magic], eax
+    mov [rip + mb2_ptr], rbx
+
+    // Load pamOS page map
+    mov cr3, rcx
+
+    // Configure stack
+    mov rsp, 0x19fff
+
+    // Load GDT included in our pages
+    lgdt gdtr_ptr
+
+.reloadCS:
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    mov eax, [rip + mb2_magic]
+    mov rbx, [rip + mb2_ptr]
+    mov edi, eax
+    mov esi, ebx
+
+    movabs rax, offset kmain
+    jmp rax
+_loop:
+    jmp _loop
+.popsection
+
+.pushsection .rodata, "a"
+gdt:
+    .zero 4  // 0th entry null
+    // Set Long-mode flag (53), present bit (47), descriptor type bit (44),
+    // exec bit (43)
+    .quad (1 << 53) | (1 << 47) | (1 << 44) | (1 << 43)
+gdtr_ptr:
+    // Size - 1
+    .word . - gdt - 1
+    .quad gdt
+.popsection
