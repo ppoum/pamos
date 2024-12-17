192fe4650b3e44135f99ed4d62d83ef107576b85
ppoum <pam@poum.ca>
2024-12-16 21:01:58 -0500

    Parse MB2 boot info in kmain and fill FB
    If the framebuffer tag is found in the MB2 boot info, then use the
    information to fill the screen with a rainbow to ensure values are
    correct.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@
 
 use core::arch::global_asm;
 
+use boot::multiboot2::{self, FramebufferTag, TagCollector};
+
+mod boot;
+
 #[panic_handler]
 fn _panic_handler(_: &core::panic::PanicInfo) -> ! {
     loop {}
@@ -11,6 +15,37 @@ fn _panic_handler(_: &core::panic::PanicInfo) -> ! {
 global_asm!(include_str!("main.s"));
 
 #[no_mangle]
-pub extern "C" fn kmain(_magic: u32, _mb2_ptr: u32) -> ! {
+pub extern "C" fn kmain(magic: u32, mb2_ptr: usize) -> ! {
+    if magic != boot::multiboot2::EAX_MB2_MAGIC {
+        panic!("No MB2 information")
+    }
+
+    // SAFETY: Magic in first arg, 2nd arg should be a valid pointer
+    let boot_info = unsafe { multiboot2::BootInfo::try_from_addr(mb2_ptr).unwrap() };
+    let tags = TagCollector::try_new(&boot_info).unwrap();
+
+    if let Some(FramebufferTag {
+        horizontal_res,
+        vertical_res,
+        scanline_size,
+        address,
+        ..
+    }) = tags.framebuffer
+    {
+        for i in 0..*vertical_res as usize {
+            for j in 0..*horizontal_res as usize {
+                let pixel_offset = j + (*scanline_size as usize * i);
+                let addr = *address as usize + 4 * pixel_offset;
+
+                let r = (255 * j / *horizontal_res as usize) as u32;
+                let b = (255 * i / *vertical_res as usize) as u32;
+                // Assume RGB for now
+                let color = (r << 16) + b;
+                // let color = (255 << 16) + 255;
+                unsafe { *(addr as *mut u32) = color };
+            }
+        }
+    }
+
     loop {}
 }
